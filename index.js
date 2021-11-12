c76f81abddd5a751470860b4968ea7a79cbd84b2
natan1563 <natromafe@gmail.com>
2021-11-12 12:12:02 -0300

    Configs basicas

--- index.js
+++ index.js
@@ -7,6 +7,50 @@ const flash = require('express-flash')
 const app = express()
 const conn = require('./db/conn')
 
+app.engine('handlebars', exphbs())
+app.set('view engine', 'handlebars')
+
+// receber a resposta do body
+
+app.use(
+  express.urlencoded({extended: true})
+)
+
+app.use(express.json())
+
+app.use(
+  session({
+    name: 'session',
+    secret: 'nosso_secret',
+    resave: false,
+    saveUninitialized: false,
+    store: new FileStore({
+      logFn: () => {},
+      path: require('path').join(require('os').tmpdir(), 'sessions')
+    }),
+    cookie: {
+      secure: false,
+      maxAge: 360000,
+      expires: new Date(Date.now() + 360000),
+      httpOnly: true
+    }
+  })
+)
+
+// flash messages 
+app.use(flash())
+
+// public path 
+app.use(express.static('public'))
+
+app.use((req, res, next) => {
+  if (req.session.userId) {
+    res.locals.session = req.session 
+  }
+
+  next()
+})
+
 conn
   .sync()
   .then(() => {
