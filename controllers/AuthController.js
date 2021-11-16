061746dce07d6eb6be8ea7021519918a47c58b4d
natan1563 <natromafe@gmail.com>
2021-11-15 22:34:37 -0300

    Criando autenticação

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -6,6 +6,40 @@ module.exports = class AuthController {
     res.render('auth/login')
   }
 
+  static async loginPost(req, res) {
+    const {email, password} = req.body
+
+    // find user validation
+    const user = await User.findOne({where: {email}})
+
+    if (!user) {
+      req.flash('message', 'Usuário não encontrado')
+      res.render('auth/login')
+      return
+    }
+
+    // check if password match
+    const passwordMatch = bcrypt.compareSync(password, user.password)
+
+    if (!passwordMatch) {
+      req.flash('message', 'Senha inválida')
+      res.render('auth/login')
+      return
+    }
+
+    try {
+      const createdUser = await User.create(user)
+      console.log(user)
+      req.session.userid = user.id
+
+      req.session.save(() => {
+        res.redirect('/')
+      })
+    } catch(err) {
+      console.log(err)
+    }
+  }
+
   static register(req, res) {
     res.render('auth/register')
   }
@@ -50,5 +84,10 @@ module.exports = class AuthController {
     } catch(err) {
       console.log(err)
     }
+  }
+
+  static logout(req, res) {
+    req.session.destroy()
+    res.redirect('/login')
   }
 }
--- db/conn.js
+++ db/conn.js
@@ -1,5 +1,5 @@
 const { Sequelize } = require('sequelize')
-const sequelize = new Sequelize('toughts', 'root', 'toor', {
+const sequelize = new Sequelize('toughts', 'root', 'root', {
   host: 'localhost',
   dialect: 'mysql'
 })
