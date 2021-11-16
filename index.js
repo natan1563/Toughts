6b195c332c6b71e65fd374cc6f7196e1f19fd295
natan1563 <natromafe@gmail.com>
2021-11-15 23:29:46 -0300

    Estilos da dashboard

--- index.js
+++ index.js
@@ -40,8 +40,8 @@ app.use(
     }),
     cookie: {
       secure: false,
-      maxAge: 360000,
-      expires: new Date(Date.now() + 360000),
+      maxAge: 36000000,
+      expires: new Date(Date.now() + 36000000),
       httpOnly: true
     }
   })
