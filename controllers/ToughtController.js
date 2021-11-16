8de0d142758ed323fbf90f1f9ab0efa3d044a563
natan1563 <natromafe@gmail.com>
2021-11-15 23:40:03 -0300

    Adicionando a ação de excluir pensamento

--- controllers/ToughtController.js
+++ controllers/ToughtController.js
@@ -53,4 +53,26 @@ module.exports = class ToughtController {
       console.log(error)
     }
   }
+
+  static async removeTought (req, res) {
+    const id = req.body.id 
+    const UserId = req.session.userid
+
+    try {
+      await Tought.destroy({
+        where: {
+          id,
+          UserId
+        }
+      })
+
+      req.flash('message', 'Pensamento removido com sucesso!')
+  
+      req.session.save(() => {
+        res.redirect('/toughts/dashboard')
+      })
+    } catch (error) {
+      console.log(error)
+    }
+  }
 }
